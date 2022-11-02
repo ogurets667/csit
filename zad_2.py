dfe377a0cf585d4d3471fabcba0d32e861f4a710
ogurets667 <dangerousguy1337@gmail.com>
2022-11-03 00:47:52 +0400

    Fourth commit

--- zad_2.py
+++ zad_2.py
@@ -1,9 +1,11 @@
 print("Hello world!")
-def fib(n):
-	if n == 1:
-		return 0
-	if n == 2:
-		return 1
-	return fib(n-1) + fib(n-2)
-n = int(input())
-print(fib(n))
+a = 0
+b = 1
+c = 1
+for i in range (int(input())):
+        print(a)
+        a = b
+        b = c
+        c = a + b
+
+        
